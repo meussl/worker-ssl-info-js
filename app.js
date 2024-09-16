fc7c37fc7419faf11cc9e555e728f2337cea943d
Andre Kutianski <andre@eunarede.com>
2024-09-16 19:31:59 -0300

    refactor: refact root certificates

--- app.js
+++ app.js
@@ -1,58 +1,18 @@
 const express = require('express');
-const https = require('https');
 const sslChecker = require('./sslChecker'); // Correctly import the sslChecker function
 const issuers = require('./issuers'); // Import the issuers data
 const app = express();
-const { exec } = require('child_process');
-const fs = require('fs');
-const { Certificate } = require('@fidm/x509');
-
-
-function getRootCertificates() {
-    return new Promise((resolve, reject) => {
-        exec('ls /etc/ssl/certs/*.pem', (err, stdout, stderr) => {
-            if (err) {
-                return reject(`Error listing certificates: ${stderr}`);
-            }
-
-            const certFiles = stdout.split('\n').filter(file => file);
-            const certs = [];
-
-            certFiles.forEach(file => {
-                const pem = fs.readFileSync(file, 'utf8');
-                const cert = Certificate.fromPEM(Buffer.from(pem));
-
-                // Check if the certificate uses RSA
-                if (cert.publicKey.algo === 'rsaEncryption') {
-                    const sans = cert.subjectAltName ? cert.subjectAltName.map(altName => altName.value) : [];
-
-                    certs.push({
-                        // subject: cert.subject,
-                        issuer: cert.issuer,
-                        valid_from: cert.validFrom,
-                        valid_to: cert.validTo,
-                        serialNumber: cert.serialNumber,
-                        fingerprint: cert.fingerprint,
-                        // sans: sans
-                    });
-                }
-            });
-
-            resolve(certs);
-        });
-    });
-}
+const getRootCertificates = require("./getRootCertificates");
 
 app.get('/ssl-info', async (req, res) => { // Make the handler function async
     const domain = req.query.domain;
     if (!domain) {
-        return res.status(400).json({ error: 'Domain parameter is required' });
+        return res.status(400).json({error: 'Domain parameter is required'});
     }
 
     try {
         const {ssl, issuer} = await sslChecker(domain, {validateSubjectAltName: true}); // Await the sslChecker function
-        
-        
+
         res.json({
             domain: domain,
             status: ssl.valid ? 'valid' : 'invalid',
@@ -62,7 +22,7 @@ app.get('/ssl-info', async (req, res) => { // Make the handler function async
             requestEpoch: Date.now()
         });
     } catch (error) {
-        res.status(500).json({ error: 'Request error', message: error.message });
+        res.status(500).json({error: 'Request error', message: error.message});
     }
 });
 
@@ -77,7 +37,7 @@ app.get('/root-certificates', async (req, res) => {
         res.set('Cache-Control', 'public, max-age=3600'); // Set cache headers
         res.json(certs);
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        res.status(500).json({error: error.message});
     }
 });
 
---
+++ getRootCertificates.js
@@ -0,0 +1,40 @@
+import {exec} from "child_process";
+import fs from "fs";
+import {Certificate} from "@fidm/x509";
+
+function getRootCertificates() {
+    return new Promise((resolve, reject) => {
+        exec('ls /etc/ssl/certs/*.pem', (err, stdout, stderr) => {
+            if (err) {
+                return reject(`Error listing certificates: ${stderr}`);
+            }
+
+            const certFiles = stdout.split('\n').filter(file => file);
+            const certs = [];
+
+            certFiles.forEach(file => {
+                const pem = fs.readFileSync(file, 'utf8');
+                const cert = Certificate.fromPEM(Buffer.from(pem));
+
+                // Check if the certificate uses RSA
+                if (cert.publicKey.algo === 'rsaEncryption') {
+                    const sans = cert.subjectAltName ? cert.subjectAltName.map(altName => altName.value) : [];
+
+                    certs.push({
+                        // subject: cert.subject,
+                        issuer: cert.issuer,
+                        valid_from: cert.validFrom,
+                        valid_to: cert.validTo,
+                        serialNumber: cert.serialNumber,
+                        fingerprint: cert.fingerprint,
+                        // sans: sans
+                    });
+                }
+            });
+
+            resolve(certs);
+        });
+    });
+}
+
+export default getRootCertificates;
