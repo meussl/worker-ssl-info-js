0faee732277a59d4cb797d95535c24b7bb09a73e
Andre Kutianski <andre@eunarede.com>
2024-09-16 19:32:42 -0300

    feat: add user agent header

--- sslChecker.js
+++ sslChecker.js
@@ -2,29 +2,32 @@ const http = require("http");
 const https = require("https");
 
 const checkPort = (port) =>
-  !isNaN(parseFloat(port)) && Math.sign(port) === 1;
+    !isNaN(parseFloat(port)) && Math.sign(port) === 1;
 
 const getDaysBetween = (validFrom, validTo) =>
-  Math.round(Math.abs(+validFrom - +validTo) / 8.64e7);
+    Math.round(Math.abs(+validFrom - +validTo) / 8.64e7);
 
 const getDaysRemaining = (validFrom, validTo) => {
-  const daysRemaining = getDaysBetween(validFrom, validTo);
+    const daysRemaining = getDaysBetween(validFrom, validTo);
 
-  if (new Date(validTo).getTime() < new Date().getTime()) {
-    return -daysRemaining;
-  }
+    if (new Date(validTo).getTime() < new Date().getTime()) {
+        return -daysRemaining;
+    }
 
-  return daysRemaining;
+    return daysRemaining;
 };
 
 const DEFAULT_OPTIONS = {
-  agent: new https.Agent({
-    maxCachedSessions: 0,
-  }),
-  method: "HEAD",
-  port: 443,
-  rejectUnauthorized: false,
-  validateSubjectAltName: true
+    agent: new https.Agent({
+        maxCachedSessions: 0,
+    }),
+    method: "GET",
+    port: 443,
+    rejectUnauthorized: false,
+    validateSubjectAltName: true,
+    headers: {
+        "User-Agent": "MeuSSL Bot 1.0",
+    }
 };
 
 // Propriedades Detalhadas
@@ -57,65 +60,73 @@ const DEFAULT_OPTIONS = {
 // Essas são algumas das propriedades mais comuns que você pode acessar no objeto retornado por getPeerCertificate.
 
 const sslChecker = (host, options = {}) =>
-  new Promise((resolve, reject) => {
-    options = Object.assign({}, DEFAULT_OPTIONS, options);
+    new Promise((resolve, reject) => {
+        options = Object.assign({}, DEFAULT_OPTIONS, options);
 
-    if (!checkPort(options.port)) {
-      reject(Error("Invalid port"));
-      return;
-    }
+        if (!checkPort(options.port)) {
+            reject(Error("Invalid port"));
+            return;
+        }
 
-    try {
-        const req = https.request(
-          { host, ...options },
-          (res) => {
-            let { valid_from, valid_to, subjectaltname, issuer, subject, fingerprint256, serialNumber } = res.socket.getPeerCertificate();
-            res.socket.destroy();
+        try {
+            const req = https.request(
+                {host, ...options},
+                (res) => {
+                    let {
+                        valid_from,
+                        valid_to,
+                        subjectaltname,
+                        issuer,
+                        subject,
+                        fingerprint256,
+                        serialNumber
+                    } = res.socket.getPeerCertificate();
+                    res.socket.destroy();
 
-            if (!valid_from || !valid_to || !subjectaltname) {
-              reject(new Error("No certificate"));
-              return;
-            }
+                    if (!valid_from || !valid_to || !subjectaltname) {
+                        reject(new Error("No certificate"));
+                        return;
+                    }
 
-            const validTo = new Date(valid_to);
-            const validFor = subjectaltname
-              .replace(/DNS:|IP Address:/g, "")
-              .split(", ");
+                    const validTo = new Date(valid_to);
+                    const validFor = subjectaltname
+                        .replace(/DNS:|IP Address:/g, "")
+                        .split(", ");
 
-              const issuerInfo = {
-                commonName: issuer.CN,
-                organization: issuer.O,
-                organizationalUnit: issuer.OU,
-                locality: issuer.L,
-                state: issuer.ST,
-                country: issuer.C
-              };
+                    const issuerInfo = {
+                        commonName: issuer.CN,
+                        organization: issuer.O,
+                        organizationalUnit: issuer.OU,
+                        locality: issuer.L,
+                        state: issuer.ST,
+                        country: issuer.C
+                    };
 
-              const sslInfo = {
-                commonName: issuer.CN,
-                subject: subject,
-                daysRemaining: getDaysRemaining(new Date(), validTo),
-                valid: res.socket.authorized || false,
-                validFrom: new Date(valid_from).toISOString(),
-                validTo: validTo.toISOString(),
-                validFor,
-                fingerprint256: fingerprint256,
-                serialNumber: serialNumber
-              };
-  
-              resolve({ issuer: issuerInfo, ssl: sslInfo });
-          }
-        );
+                    const sslInfo = {
+                        commonName: issuer.CN,
+                        subject: subject,
+                        daysRemaining: getDaysRemaining(new Date(), validTo),
+                        valid: res.socket.authorized || false,
+                        validFrom: new Date(valid_from).toISOString(),
+                        validTo: validTo.toISOString(),
+                        validFor,
+                        fingerprint256: fingerprint256,
+                        serialNumber: serialNumber
+                    };
 
-        req.on("error", reject);
-        req.on("timeout", () => {
-          req.destroy();
-          reject(new Error("Timed Out"));
-        });
-        req.end();
-    } catch (e) {
-      reject(e);
-    }
-  });
+                    resolve({issuer: issuerInfo, ssl: sslInfo});
+                }
+            );
+
+            req.on("error", reject);
+            req.on("timeout", () => {
+                req.destroy();
+                reject(new Error("Timed Out"));
+            });
+            req.end();
+        } catch (e) {
+            reject(e);
+        }
+    });
 
 module.exports = sslChecker;
