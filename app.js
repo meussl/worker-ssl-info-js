1abc3d17415d56c210484d1e1cefa29d38381cb7
Andre Kutianski <andre@eunarede.com>
2024-09-26 21:13:26 -0300

    feat: add domain resolution test
    Added domain resolution test before try to get ssl certificate

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const sslChecker = require('./sslChecker'); // Correctly import the sslChecker f
 const issuers = require('./issuers'); // Import the issuers data
 const app = express();
 const getRootCertificates = require("./getRootCertificates");
+const dns = require('dns');
 
 app.get('/ssl-info', async (req, res) => { // Make the handler function async
     const domain = req.query.domain;
@@ -10,19 +11,33 @@ app.get('/ssl-info', async (req, res) => { // Make the handler function async
         return res.status(400).json({error: 'Domain parameter is required'});
     }
 
+    console.log(`Checking SSL for ${domain}`);
+
     try {
+        await dns.promises.resolve(domain);
+    } catch (err) {
+        return res.status(400).json({error: 'Domain does not properly resolve, this is alive?'});
+    }
+
+    try {
+
         const {ssl, issuer} = await sslChecker(domain, {validateSubjectAltName: true}); // Await the sslChecker function
 
-        res.json({
+        let response = {
             domain: domain,
             status: ssl.valid ? 'valid' : 'invalid',
             ssl: ssl,
             issuer: issuer,
             requestTime: new Date().toISOString(),
             requestEpoch: Date.now()
-        });
+        };
+
+        console.log(response);
+
+
+        res.json(response);
     } catch (error) {
-        res.status(500).json({error: 'Request error', message: error.message});
+        res.status(404).json({error: 'Request error', message: error.message});
     }
 });
 
@@ -41,7 +56,7 @@ app.get('/root-certificates', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
